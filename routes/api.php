77c0b1283fa720d43f9a93bd511a176a80f9b7e6
mohamad-slime <mohamadamin_dastmozdraki@outlook.com>
2025-11-10 00:05:23 +0330

    Refactor API routes for improved organization; move login route and clean up task routes

--- routes/api.php
+++ routes/api.php
@@ -5,23 +5,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
 
-
 Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('login', [AuthController::class, 'login'])->name('login');
+
+    // Auth routes
     Route::post('logout', [AuthController::class, 'logout']);
     Route::get('me', [AuthController::class, 'me']);
-});
 
-
-Route::middleware('auth:sanctum')->group(function () {
+    // Task routes
     Route::get('tasks', [TaskController::class, 'index']);
     Route::post('tasks', [TaskController::class, 'store']);
     Route::get('tasks/{id}', [TaskController::class, 'show']);
     Route::put('tasks/{id}', [TaskController::class, 'update']);
     Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
-
-    Route::get('tasks/{id}/status', [TaskController::class, 'status']);
-    Route::put('tasks/{id}/status', [TaskController::class, 'updateStatus']);
-    
 });
